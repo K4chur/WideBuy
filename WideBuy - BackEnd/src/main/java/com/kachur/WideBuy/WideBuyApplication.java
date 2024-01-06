679b132f415726f36ec204e356eea904f5359eee
Vadim Voichenko <vvojchenko0@gmail.com>
2024-01-06 17:19:42 +0200

    Initial commit

---
+++ WideBuy - BackEnd/src/main/java/com/kachur/WideBuy/WideBuyApplication.java
@@ -0,0 +1,13 @@
+package com.kachur.WideBuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WideBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WideBuyApplication.class, args);
+	}
+
+}
---
+++ WideBuy - BackEnd/src/test/java/com/kachur/WideBuy/WideBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kachur.WideBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WideBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
