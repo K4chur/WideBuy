e7d706decb8b6ec17ae27b5c20f4ce429e0880ba
Vadim Voichenko <vvojchenko0@gmail.com>
2024-01-06 20:34:30 +0200

    Configured cors origins and unavailable httpMethods

---
+++ WideBuy - BackEnd/src/main/java/com/kachur/WideBuy/config/RestConfig.java
@@ -0,0 +1,41 @@
+package com.kachur.WideBuy.config;
+
+import com.kachur.WideBuy.entity.Product;
+import com.kachur.WideBuy.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.stream.Collectors;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+    private final EntityManager entityManager;
+    private final HttpMethod[] unavailableMethods = {HttpMethod.PATCH, HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+    public RestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+
+        closeHttpExposure(config, Product.class);
+        closeHttpExposure(config, ProductCategory.class);
+
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toList().toArray(new Class[0]));
+        cors.addMapping("/wide_buy/**").allowedOrigins("http://localhost:4200");
+    }
+
+    private void closeHttpExposure(RepositoryRestConfiguration config, Class someClass) {
+        config.getExposureConfiguration()
+                .forDomainType(someClass)
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unavailableMethods)))
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unavailableMethods)));
+    }
+}
